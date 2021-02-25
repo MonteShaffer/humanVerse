75bd2fb782e229cbe12b1c5d8e61eaa5a7343066
MonteShaffer <monte.shaffer@gmail.com>
2021-02-25 01:39:10 -0800

    colors (6)

--- humanVerse/R/functions-file.R
+++ humanVerse/R/functions-file.R
@@ -104,11 +104,11 @@ includeRemoteFiles = function(urls, verbose=FALSE, ...)
     if(verbose) { cat("\t ... ",myfile); } else { cat("\t ... ",basename(myfile),"\n"); }
     }
   }
-  
+
 
 installGithubLibrary = function()
 	{
-	
+
 	#
 	# {
   # "id": 294247360,
@@ -116,14 +116,14 @@ installGithubLibrary = function()
   # "name": "humanVerseWSU",
   # "full_name": "MonteShaffer/humanVerseWSU",
   # "private": false,
-  
-  
+
+
 	# https://api.github.com/
 	# "repository_url": "https://api.github.com/repos/{owner}/{repo}",
 	# "repository_url": "https://api.github.com/repos/MonteShaffer/humanVerseWSU",
-	
-	# check out remotes ... 
-	
+
+	# check out remotes ...
+
 	# remote_download.github_remote <- function(x, quiet = FALSE) {
   # if (!quiet) {
     # message("Downloading GitHub repo ", x$username, "/", x$repo, "@", x$ref)
@@ -134,32 +134,33 @@ installGithubLibrary = function()
   # src <- paste0(src_root, "/tarball/", utils::URLencode(x$ref, reserved = TRUE))
 
   # download(dest, src, auth_token = x$auth_token)
-  
-  
-  
-  
-  
+
+
+
+
+
   # github_remote <- function(repo, ref = "HEAD", subdir = NULL,
                        # auth_token = github_pat(), sha = NULL,
                        # host = "api.github.com", ...) {
 
   # meta <- parse_git_repo(repo)
   # meta <- github_resolve_ref(meta$ref %||% ref, meta, host = host, auth_token = auth_token)
-	
+
 	}
- 
+
 includeGithubFolder = function(url, ...)  # pattern = "[.][RrSsQq]$",
 	{
-	# args = grabFunctionParameters();
-	args = .GlobalEnv$.args = grabFunctionParameters();
+	args = grabFunctionParameters();
+        # TRUE would store "last" in some memory (GLOBAL SCOPE)
+	# args = .GlobalEnv$.args = grabFunctionParameters();
 	# print(args);
-	
+
 	# stop("monte");
-	
+
 	force.cache = FALSE;  # maybe move as a parameter ???
 	if(!force.cache)
 		{
-		# may live in ... as force.download ... 
+		# may live in ... as force.download ...
 		if(exists("args"))
 			{
 			if(exists(".dots.keys.", where=args))
@@ -172,48 +173,49 @@ includeGithubFolder = function(url, ...)  # pattern = "[.][RrSsQq]$",
 				}
 			}
 		}
-	
+
 	cat("\n", "force.cache ... ", force.cache, "\n\n");
-	
+
 	html.local = getRemoteAndCache(url, ...);
 	html.cache = gsub(".html", ".cache", html.local);
-	
+
 	if(file.exists(html.cache) && !force.cache)
 		{
 		cat("\n", "============", "GRABBING FROM CACHE", "============", "\n");
-		links = as.character( unlist( readFromPipe(html.cache, header=FALSE) ) );		
-		} else {	
+		links = as.character( unlist( readFromPipe(html.cache, header=FALSE) ) );
+		} else {
+		    cat("\n", "============", "DOWNLOADING DIRECTORY PAGE", "============", "\n");
 				html.str = readStringFromFile(html.local);
-				
+
 				github.base = "https://github.com/";
 				github.raw = "https://raw.githubusercontent.com/";
-				
+
 				# MonteShaffer/humanVerse/tree/main/humanVerse/R/
 				# MonteShaffer/humanVerse/blob/main/humanVerse/R/functions-colors.R
-				
-				
+
+
 				# https://raw.githubusercontent.com/MonteShaffer/humanVerse/main/humanVerse/R/globals.R
-				
-				
+
+
 				html.search = gsub(github.base, "", url, fixed = TRUE);
 				html.search = gsub("/tree/", "/blob/", html.search, fixed = TRUE);
-				
+
 				html.keys = explodeMe(html.search, html.str);
 				n = length(html.keys)
-				
+
 				html.raw = paste0(github.raw, gsub("/blob/", "/", html.search, fixed = TRUE) );
-				
+
 				links = c();
 				for(i in 2:n)
 					{
 					str = html.keys[i];
 						link = paste0(html.raw, explodeMe("\"",str)[1]);
 					links = c(links, link);
-					}	
+					}
 				storeToPipe(as.data.frame(links), html.cache, header=FALSE);
 				}
-				
-	includeRemoteFiles(links, ...);	
+
+	includeRemoteFiles(links, ...);
 	}
 
 
@@ -228,12 +230,12 @@ readStringFromFile = function(myFile, n = NULL, method ="readChar", source = "lo
 		} else {
 				if(is.null(n)) { n = if(method == "readLines") { n = -1; } else { n = file.info(myFile)$size; } }
 				}
-	
+
 	if(method == "readLines")
 		{
 		readLines(myFile, n);
 		} else 	{
-				readChar(myFile, n);	
+				readChar(myFile, n);
 				}
 	}
 
@@ -242,12 +244,15 @@ readStringFromFile = function(myFile, n = NULL, method ="readChar", source = "lo
 
 getRemoteAndCache = function(remote, local.file = NULL,
     tmp.folder = "/humanVerse/cache/", force.download = FALSE,
-    md5.hash = FALSE)
+    verbose = FALSE, md5.hash = FALSE)
   {
   useTEMP = FALSE;
   trailingSlash = (substr.neg(remote) == "/");
-  cat("\n", "remote ... ", remote, "\n\n");
-  cat("\n", "force.download ... ", force.download, "\n\n");
+  if(verbose)
+    {
+    cat("\n", "remote ... ", remote, "\n\n");
+    cat("\n", "force.download ... ", force.download, "\n\n");
+    }
   if(!is.null(local.file))
     {
     localpath = dirname(local.file);
@@ -274,19 +279,22 @@ getRemoteAndCache = function(remote, local.file = NULL,
             myfile = paste0(mypath,"/",filestem);
             }
 
-  cat("\n", "myfile ... ", myfile, "\n\n");
-   
+  if(verbose)
+    {
+    cat("\n", "myfile ... ", myfile, "\n\n");
+    }
+
   if(force.download)
     {
     if(file.exists(myfile))
       {
-	  mypath.b = paste0(mypath, "/.backup/");  createDirectoryRecursive(mypath.b);
-	  myfile.b = paste0(mypath.b, "/", filestem, "-", as.integer(Sys.time()) );
-	  
-      # file.copy(myfile, myfile.b);  # this is not file.move, doesn't exist	  
+	    mypath.b = paste0(mypath, "/.backup/");  createDirectoryRecursive(mypath.b);
+	    myfile.b = paste0(mypath.b, "/", filestem, "-", as.integer(Sys.time()) );
+
+      # file.copy(myfile, myfile.b);  # this is not file.move, doesn't exist
 	  # unlink(myfile);
-	  
-	  moveFile(myfile, myfile.b);
+
+	    moveFile(myfile, myfile.b);
       }
     }
   if(!file.exists(myfile))
@@ -299,12 +307,12 @@ getRemoteAndCache = function(remote, local.file = NULL,
 
 moveFile = function(src, dest, unlink=TRUE)
 	{
-	file.copy(src, dest);  # there is no file.move ???	  
+	file.copy(src, dest);  # there is no file.move ???
 	unlink(src);
 	}
 
 
-downloadFile = function(remote, myfile, n=(2^31 - 1), quiet = TRUE, ...)  # n could be 2^31 - 1 
+downloadFile = function(remote, myfile, n=(2^31 - 1), quiet = TRUE, ...)  # n could be 2^31 - 1
   {
   if(isTRUE(capabilities("libcurl")))
     {
@@ -339,7 +347,7 @@ http.headers = function(remote)
 http.status = function(headers)
   {
   # requires libcurl
-  attributes(headers)$status;
+  attributes(headers)$status;  # getAttribute
   }
 
 
