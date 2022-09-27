722615498ee8703da3a2c5fa81a26df8d635b5b9
MonteShaffer <monte.shaffer@gmail.com>
2022-09-27 18:58:18 -0400

    Moving data to own library

--- zz_backup_zz/-cpp-/hex.h
+++
@@ -1,2 +0,0 @@
-Rcpp::List CPL_hex_to_raw(Rcpp::CharacterVector cx);
-Rcpp::CharacterVector CPL_raw_to_hex(Rcpp::RawVector raw);
--- zz_backup_zz/misc/-stuff-/monte-april-2020/functions-compare.R
+++
@@ -1,6 +0,0 @@
-# https://www.bioinformatics.babraham.ac.uk/training/Core_R_Plotting/Core%20R%20Plotting.pdf
-
-# https://www.r-bloggers.com/2014/11/an-xpd-tion-into-r-plot-margins/
-
-
-
