1abd076df4d0313168a8d035a618d1c9670df10a
MonteShaffer <monte.shaffer@gmail.com>
2022-09-30 12:54:49 -0400

    Update functions-ini.R
    Ready to test MULTILINE

--- humanVerse/R/functions-ini.R
+++ humanVerse/R/functions-ini.R
@@ -1,29 +1,21 @@
 
 
-# write a new readString function that doesn't require a length ... all of it by default ... 
+ini.file 	= "C:/_git_/github/MonteShaffer/humanVerse/humanVerse/inst/R/config/system/constants.ini";
 
-ini.file = "C:/_git_/github/MonteShaffer/humanVerse/humanVerse/inst/R/config/system/constants.ini";
+ini.file 	= "C:/_git_/github/MonteShaffer/humanVerse/humanVerse/inst/R/config/system/humanVerse.ini";
 
-# inistr = readChars(ini.file, 8888);
-# inistr = readChars(ini.file, 9999);
-
-inistr = readTextFile(ini.file);
-lines = str.explode("\r\n", inistr);
+inistr 		= readTextFile(ini.file);
+lines 		= str.explode("\r\n", inistr);
 
 
-#lines = lines[1:33];
 
 ini.parse = function(lines, verbose=FALSE, ignore.eval = FALSE)
 	{
 	envir = environment();
 	
-	
-	
 	# if I get multiline string parsing working, adding 
 	# multiline comments should not be difficult .... 
 	# what about // comments ... two characters long , nice to have 
-	
-	
 	
 	EMPTY			= "";
 	COMMA 			= ","; 
@@ -48,7 +40,7 @@ ini.parse = function(lines, verbose=FALSE, ignore.eval = FALSE)
 	pval 			= "";
 	cparent 		= "";
 	
-	main = function() {}
+.__main__. = function() {}
 ############################### MAIN #######################
 	for(line in lines)
 		{
@@ -67,14 +59,23 @@ gggassign("RES", RES);
 		
 		line_ = str.trim(line);
 		first = charAt(line_,1);
+
+
+.__skip = function() {} 		
+
 		# SKIP LINES ...
 		if(first %in% COMMENTS) { next; }
 		if((CONTINUE_KEY == EMPTY) && (first == EMPTY)) { next; }
+
+
+.__hardstop = function() {} 		
 		
 		# stop here ... HARDSTOP =====>
 		# ===========> HARDSTOP 
 		# <=========== HARDSTOP 
 		if(first == "=" || first == "<") { break;}
+
+.__header = function() {} 		
 		
 		# WE HAVE A HEADER 
 		if(first == "[")  # not equal to anything ...  
@@ -88,6 +89,8 @@ gggassign("RES", RES);
 			}
 		
 		# WE HAVE A LINE ... LIKELY a KEY/VALUE PAIR 
+.__normal = function() {} 		
+
 		if(COMMENT_KEY == "")	
 			{ 
 			# "%~=%" = is.equal;  ;; this "=" is a problem 
@@ -96,22 +99,20 @@ gggassign("RES", RES);
 			if(ne > 1)
 				{
 				rkey = parse.walkTheLine(line, COMMENTS);
+.__equal.sign.issue = function() {} 		
 				
 				info = str.remainder(rkey, line);
 				rval = str.remainder("=", info);
-				info = str.remaindere(
-				info = str.explode(rkey, line)[2];
-				# walk to first equal ... str.pos / explode ... one function?
-				pos = str.pos("=", info); len = str.len(info);
-				rval = substring(info, pos+1, len);
-				# get special key ... (contains equal)
-				} else {		
+				} else {	
+						# ne == 1 ... 
 						info = str.explode("=", line);
 						rkey = str.trim(info[1]);
 						rval = str.trim(info[2]);
 						}
 			
 			hasMemory = hasRcode = FALSE;
+.__memory.eval = function() {} 		
+
 			# allows for a bit of white space between ^ = R 
 			if( str.contains(MEMORY_STORE, paste0(rkey,"=")) ) 
 				{ 
@@ -124,10 +125,12 @@ gggassign("RES", RES);
 				rval = str.trim(str.begin("R", rval, trim=TRUE));
 				}
 		 
-normal = function() {} 		
+	
 
 			key = ini.cleanKey(rkey);  # raw key 
-			val = parse.walkTheLine(rval, COMMENTS); 
+			val = parse.walkTheLine(rval, COMMENTS);
+
+.__normal.GTG = function() {} 				
 			fin = property.get("more", val); # are we finished?
 			GTG = FALSE; # I am good to go ... multiline may stop this ... 
 			if(is.null(fin)) { GTG = TRUE; } else { CONTINUE_KEY = key; }
@@ -153,13 +156,14 @@ normal = function() {}
 		# we keep walking lines until the string TERMINATES
 		# we set CURRENT_KEY to ""
 		# we evaluate and move on ...
-multiline = function() {} 		
+.__multiline = function() {} 		
 		rval = line;  # grab everything (whitespace);
 		
 		# more = "pval";  # pval that grows ... 
 		val = parse.walkTheLine(rval, COMMENTS, fin);
 		fin = property.get("more", val); # are we finished?
 
+.__multiline.GTG = function() {} 	
 			GTG = FALSE; # I am good to go ... multiline may stop this ... 
 			if(is.null(fin)) { GTG = TRUE; }
 			if(GTG)
@@ -253,294 +257,3 @@ ini.cleanKey = function(key)
 	
 	
 	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-# multiline logic is messing it up... skip for now ... 
-
-parse.ini = function(lines, verbose=FALSE)
-	{
-# verbose=FALSE;
-	# KISS ... store keys as | = val ...
-
-	COMMA = ","; 
-	COMMENT		= "#";
-	COMMENT_INI	= ";";
-	COMMENTS	= c(COMMENT,COMMENT_INI,COMMA);
-	MEMORY_STORE = "^=";
-	EVAL_RCODE = "=R";
-	
-	SINGLE_QUOTE 		= "'";
-	DOUBLE_QUOTE 		= '"';
-
-	
-	CONTINUE_KEY = "";
-	MEMORY = list();
-	RES = list();
-	line.no = 0;
-	pkey = "";
-	pval = "";
-	pparent = cparent = "";
-	continue = NULL;
-	
-	envir = environment();
-	
-	prepKeyVal = function()
-		{
-if(verbose)
-	{
-cat("\n\n prepKeyVal() \n\n");
-	} 
-		if(CONTINUE_KEY == "")
-			{
-			info = str.explode("=", line);
-			key = str.trim(info[1]);
-			# in case they put doubles (R vs php)
-			key = str.replace("[[","[", key);  
-			key = str.replace("]]","]", key);
-			key = str.replace(c(SINGLE_QUOTE,DOUBLE_QUOTE), "", key);
-if(verbose)
-	{		
-cat("\n\n"); dput(continue); cat("\n\n");
-	}
-			val = str.implode("=", info[-1]);			
-			} else { 
-					key = CONTINUE_KEY;
-					val = line;					
-					}
-		key %TO% envir;
-		val %TO% envir;
-		}
-		
-	doNew = function()
-		{
-if(verbose)
-	{
-cat("\n\n doNew() \n\n");
-	}
-		info = parse.walkTheLine(line_, COMMENTS);
-			
-		pparent = cparent;				pparent %TO% envir;	
-		cparent = info;					cparent %TO% envir;	
-		RES[[cparent]] = list();		RES %TO% envir;	
-		pkey = pval = "";				pkey %TO% envir;	
-										pval %TO% envir;	
-
-if(verbose)
-	{
-print(RES);
-	}
-		}
-	
-	
-	checkContinue = function()
-		{
-if(verbose)
-	{
-cat("\n\n checkContinue() \n\n");
-	}
-		if(CONTINUE_KEY != "")
-			{
-			key = CONTINUE_KEY;
-			nval = pval;
-			if(str.contains("[",key))
-				{
-				RES[[cparent]][[key]] = list.append(RES[[cparent]][[key]], nval);
-				} else { RES[[cparent]][[key]] = nval; } 
-			
-			RES %TO% envir;	
-				
-			CONTINUE_KEY == "";			CONTINUE_KEY %TO% envir;
-			continue = NULL;			continue %TO% envir;
-			}
-		}
-		
-		
-	updateMultiLine = function()
-		{
-if(verbose)
-	{
-cat("\n\n updateMultiLine() with CONTINUE_KEY: ", CONTINUE_KEY, "\n\n");
-	}
-		status = ""
-		continue = property.get("extra", nval);
-		
-		truth = "0";
-		if(!is.null(continue)) { truth = "1"; }
-		if(CONTINUE_KEY != "") { truth = paste0(truth,"1"); } else {truth = paste0(truth,"0");}
-		
-		if(truth == "00")
-			{
-			# continue is null 
-			# CONTINUE KEY is EMPTY 
-			
-			pkey = key;
-			pval = nval;
-			status = "normal";
-			}
-			
-		if(truth == "01")
-			{
-			# continue is null 
-			# CONTINUE KEY is NOT EMPTY 
-			# we had a multiline that finished 
-			
-			pkey = key;
-			pval = nval;					pval %TO% envir;
-			status = "end-multiline";
-			# key = CONTINUE_KEY gets referenced inside 
-			# contine = NULL, CONTINUE_KEY = "" AFTER ....
-			checkContinue();
-			}
-			
-		if(truth == "10")
-			{
-			# continue is not null 
-			# CONTINUE KEY is EMPTY 
-			# we had a multiline that started 
-			
-			CONTINUE_KEY = key;				CONTINUE_KEY %TO% envir;
-			pkey = key;
-			pval = nval;
-			status = "started-multiline";
-			}
-			
-		if(truth == "10")
-			{
-			# continue is not null 
-			# CONTINUE KEY is NOT EMPTY 
-			# we are in the middle of a streaming multiline
-			
-			pkey = key;
-			pval = nval;
-			status = "streaming-multiline";
-			}
-			
-		key %TO% envir;
-		nval %TO% envir;
-		pkey %TO% envir;
-		pval %TO% envir;
-		return(status);
-		}
-		
-	storeKeyVal = function()
-		{
-if(verbose)
-	{
-cat("\n\n storeKeyVal() \n\n");
-	}
-		### MEMORY ELMENTS CAN ONLY BE ONE LINERS .... 
-		# can only be one liners ...
-		if(!str.contains("[",key) && str.contains("^",key))
-			{
-			key = str.trim(str.replace("^", "", key));
-			MEMORY[[key]] = nval;				MEMORY %TO% envir;
-			}
-			
-		## GRAB INFO from MEMORY with ^ operator 
-		if(str.contains("[",key) && str.contains("^",key))
-			{
-			# which element has this in it ...
-			keys = str.trim(str.replace("]","",str.explode("[", key)));
-			logic = str.contains("^", keys);
-			keys = str.replace("^", "", keys);
-				subkey = keys[logic];
-			if(!is.null(MEMORY[[subkey]]))
-				{
-				keys[logic] = MEMORY[[subkey]];
-				}
-			
-			key = str.implode("", "[" %.% keys %.% "]");
-			}
-			
-		## STORE 
-		if(str.contains("[]",key))
-			{
-			RES[[cparent]][[key]] = list.append(RES[[cparent]][[key]], nval);
-			} else { RES[[cparent]][[key]] = nval; } 
-		
-		# nval didn't change 
-		key %TO% envir;
-		RES %TO% envir;
-			
-		}
-	
-	main = function() {}
-############################### MAIN #######################
-	for(line in lines)
-		{
-		line.no %++%.;
-		
-if(verbose)
-	{
-cat("\n\n ", line.no, " --> ", line, "\n\n");
-	}
-
-if(verbose)
-	{
-xxx = readline(prompt="Press [enter] to continue, [ESC] to quit");
-gggassign("RES", RES);
-	}
-		line_ = str.trim(line);
-		first = charAt(line_,1);
-		# SKIP LINES ...
-		if(is.null(continue) && (first %in% COMMENTS || first == EMPTY)) { next; }
-		
-		
-
-# if(line.no > 20) { stop("testing"); }
-
-		if(first == "=")
-			{
-			break;  # stop here ... HARDSTOP =====>
-			}
-		
-		if(first == "[")  # not equal to anything ... 
-			{
-			doNew();
-			next;
-			}
-		
-		prepKeyVal();
-		
-		nval = parse.walkTheLine(val, COMMENTS, continue=continue);
-		
-		status = updateMultiLine();
-		
-if(verbose)
-	{
-cat("\n\n status: ", status, " key : ", key, " nval : ", nval, "\n\n");
-	}
-		if(status != "normal") { next; }
-		
-		
-		storeKeyVal();
-			
-		pkey = key;
-		pval = nval;
-
-if(verbose)
-	{		
-cat("\n\n\t\t\t ", pkey, " --> ", pval, "\n\n");
-print(str(RES));		
-print(str(MEMORY));
-cat("\n\n MDLFjkdlsj \n\n");
-	}
-		
-		}
-	RES;
-	}
-
-
-
-
