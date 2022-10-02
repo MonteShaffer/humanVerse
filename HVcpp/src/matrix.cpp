6c07cc93799371f7f1cb37ae91f8d85b74a837ac
MonteShaffer <monte.shaffer@gmail.com>
2022-10-02 16:38:01 -0400

    Update matrix.cpp
    touch point

--- HVcpp/src/matrix.cpp
+++ HVcpp/src/matrix.cpp
@@ -73,13 +73,109 @@ MatrixXd matrix_identity(int size = 5)
 	return B.Identity(size, size);
 	}
 
-// [[Rcpp::export]]
-MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double eps = 0)
+
+double calculateTolerance(Map<MatrixXd> A, double new_factor, 
+							compound=true, bool verbose=true)
+{
+	double eps 			= std::numeric_limits<double>::epsilon();
+	double old_factor 	= eps * A.diagonal().size(); 
+	std::string msg 	= "RTM: it is defined as 'diagonal size times machine epsilon' [ NumTraits<Scalar>::epsilon() ]";
+			
+	new_factor 		= fabs(new_factor); // has to be unsigned ... 
+	
+	double eps_old 	= eps * old_factor;
+	double eps_new 	= (compound) ? eps * old_factor * new_factor 
+								: eps * new_factor;
+	
+	
+// https://teuder.github.io/rcpp4everyone_en/060_printing_massages.html
+	if(verbose)
+		{
+		Rcpp::Rcout << "The old tolerance factor was : " << 
+												old_factor << "\n";
+		Rcpp::Rcout << "\t\t\t" << msg << "\n";
+		Rcpp::Rcout << "The old tolerance was : " << 
+												eps_old << "\n";
+		
+		
+		Rcpp::Rcout << "You entered as a new tolerance factor : " << 
+												new_factor << "\n";
+		
+		Rcpp::Rcout << "The system's eps [tolerance] is : " << 
+												eps << "\n";
+										
+		Rcpp::Rcout << "Now, the new tolerance for" << 
+						" this matrix operation is: " << 
+												eps_new << "\n";
+		
+		}	
+	return eps_new;
+}
+
+
+{
+double old_factor, double new_factor, double eps = 0, const std::string msg="", bool compound=true, bool verbose=true)
 	{
+	if(eps == 0) { eps = std::numeric_limits<double>::epsilon(); }
+	old_factor = fabs(old_factor); // has to be unsigned ... 
+	new_factor = fabs(new_factor); // has to be unsigned ... 
 	
+	double eps_old = eps * old_factor;
+	double eps_new = (compound) ? eps * old_factor * new_factor 
+								: eps * new_factor;
 	
+// https://teuder.github.io/rcpp4everyone_en/060_printing_massages.html
+	if(verbose)
+		{
+		Rcpp::Rcout << "The old tolerance factor was : " << 
+												old_factor << "\n";
+		Rcpp::Rcout << "\t\t\t" << msg << "\n";
+		Rcpp::Rcout << "The old tolerance was : " << 
+												eps_old << "\n";
+		
+		
+		Rcpp::Rcout << "You entered as a new tolerance factor : " << 
+												new_factor << "\n";
+		
+		Rcpp::Rcout << "The system's eps [tolerance] is : " << 
+												eps << "\n";
+										
+		Rcpp::Rcout << "Now, the new tolerance for" << 
+						" this matrix operation is: " << 
+												eps_new << "\n";
+		
+		}	
+	return eps_new;
+	}
+
+// [[Rcpp::export]]
+MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double eps_factor = -1, bool compound=true, bool verbose=true)
+	{
 	if(type == 1) { return A.inverse(); }
-	if(type == 2) 
+	if(type == 2)
+		{
+		PartialPivLU<MatrixXd> 	pplu(A); // constructor 
+		if(eps_factor > 0) 
+			{
+			double eps = calculateTolerance(eps_factor, compound, verbose);
+			double eps = std::numeric_limits<double>::epsilon();
+			// according to the docs ... 
+			double eps_old = eps * A.diagonal().size(); 
+			const std::string msg = "The documentation says it is defined as 'diagonal size times machine epsilon' [ NumTraits<Scalar>::epsilon() ]";
+			e calculateTolerance(double old_factor, double new_factor, double eps = 0, const std::string msg="", bool compound=true, bool verbose=true)
+	{
+		eps_factor
+			double eps = calculateTolerance(
+		//if(eps > 0) { pplu.setThreshold(eps); }
+		// pplu.setThreshold(1e-5);
+		/*
+		// diagonal size times machine epsilon.
+	// NumTraits<Scalar>::epsilon()
+	// If you want to come back to the default behavior, call setThreshold(Default_t)
+	*/
+		return pplu.inverse();
+		}
+	/* if(type == 2) 
 		{ 
 		PartialPivLU<MatrixXd> 	pplu;
 		if(eps > 0) { pplu.setThreshold(eps); }		
@@ -91,9 +187,9 @@ MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double eps = 0)
 		FullPivLU<MatrixXd> 	fplu;
 		if(eps > 0) { fplu.setThreshold(eps); }		
 		fplu.compute(A);
-		return fplu(A).inverse();
+		return fplu(A).inverse(); 
 		}
-
+ */
 	return A.inverse();
 	}
 
