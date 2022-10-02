f112a5f549909d6bccac8f0061827a81fdb59a76
MonteShaffer <monte.shaffer@gmail.com>
2022-10-02 18:03:42 -0400

    Update matrix.cpp
    diagonalSize

--- HVcpp/src/matrix.cpp
+++ HVcpp/src/matrix.cpp
@@ -13,9 +13,10 @@ using Eigen::PartialPivLU;
 //  Rcpp::sourceCpp("matrix.cpp", verbose=TRUE);
 
 // [[Rcpp::export]]
-VectorXd matrix_diagonal(Map<MatrixXd> M)
+VectorXd matrix_diagonal(Map<MatrixXd> M, int idx=0)
 	{
-	return M.diagonal();
+	// *this is not required to be square.	
+	return M.diagonal(idx);
 	}
 
 // [[Rcpp::export]]
@@ -75,123 +76,116 @@ MatrixXd matrix_identity(int size = 5)
 
 
 double calculateTolerance(Map<MatrixXd> A, double new_factor, 
-							compound=true, bool verbose=true)
+							bool compound=true, bool verbose=true)
 {
 	double eps 			= std::numeric_limits<double>::epsilon();
-	double old_factor 	= eps * A.diagonal().size(); 
-	std::string msg 	= "RTM: it is defined as 'diagonal size times machine epsilon' [ NumTraits<Scalar>::epsilon() ]";
-			
+	int old_factor 		= A.diagonal().size(); 
+	std::string msg 	= "RTM: *matrix eps* it is defined as 'diagonal size times machine epsilon' \n\t\t\t\t[ setThreshold( ... NumTraits<Scalar>::epsilon() ]";
+	std::string more 	= ""; 
+	if(compound) { more = " * " + std::to_string(old_factor); }
+		
+	
 	new_factor 		= fabs(new_factor); // has to be unsigned ... 
 	
-	double eps_old 	= eps * old_factor;
-	double eps_new 	= (compound) ? eps * old_factor * new_factor 
-								: eps * new_factor;
+	double eps_old	= eps * old_factor;
+	
+	double eps_new 	= (compound) 	? eps_old 	* new_factor 
+									: eps 		* new_factor;
 	
 	
 // https://teuder.github.io/rcpp4everyone_en/060_printing_massages.html
 	if(verbose)
 		{
-		Rcpp::Rcout << "The old tolerance factor was : " << 
+		Rcpp::Rcout << "\t\t\t" << msg << "\n\n";
+		
+		Rcpp::Rcout << "\tThe CPP tolerance [machine eps] : \t\t\t\t" << 
+												eps << "\n\n";
+		
+		
+		Rcpp::Rcout << "\tThe old tolerance factor was : \t\t\t\t\t" << 
 												old_factor << "\n";
-		Rcpp::Rcout << "\t\t\t" << msg << "\n";
-		Rcpp::Rcout << "The old tolerance was : " << 
-												eps_old << "\n";
-		
-		
-		Rcpp::Rcout << "You entered as a new tolerance factor : " << 
-												new_factor << "\n";
-		
-		Rcpp::Rcout << "The system's eps [tolerance] is : " << 
-												eps << "\n";
+		Rcpp::Rcout << "\tBefore, the tolerance for" << 
+						" this matrix operation *was* : \t" << 
+												eps_old << "\n\n";
+				
+				
+		Rcpp::Rcout << "\tBased on your input, the " << 
+								"new tolerance factor : \t\t" << 
+								new_factor << more << "\n";
 										
-		Rcpp::Rcout << "Now, the new tolerance for" << 
-						" this matrix operation is: " << 
-												eps_new << "\n";
+		Rcpp::Rcout << "\tNow, the new tolerance for" << 
+								" this matrix operation *is* : \t" << 
+												eps_new << "\n\n";
 		
 		}	
 	return eps_new;
 }
 
 
-{
-double old_factor, double new_factor, double eps = 0, const std::string msg="", bool compound=true, bool verbose=true)
-	{
-	if(eps == 0) { eps = std::numeric_limits<double>::epsilon(); }
-	old_factor = fabs(old_factor); // has to be unsigned ... 
-	new_factor = fabs(new_factor); // has to be unsigned ... 
-	
-	double eps_old = eps * old_factor;
-	double eps_new = (compound) ? eps * old_factor * new_factor 
-								: eps * new_factor;
-	
-// https://teuder.github.io/rcpp4everyone_en/060_printing_massages.html
-	if(verbose)
-		{
-		Rcpp::Rcout << "The old tolerance factor was : " << 
-												old_factor << "\n";
-		Rcpp::Rcout << "\t\t\t" << msg << "\n";
-		Rcpp::Rcout << "The old tolerance was : " << 
-												eps_old << "\n";
-		
-		
-		Rcpp::Rcout << "You entered as a new tolerance factor : " << 
-												new_factor << "\n";
-		
-		Rcpp::Rcout << "The system's eps [tolerance] is : " << 
-												eps << "\n";
-										
-		Rcpp::Rcout << "Now, the new tolerance for" << 
-						" this matrix operation is: " << 
-												eps_new << "\n";
-		
-		}	
-	return eps_new;
-	}
+// for SOLVE ... https://dirk.eddelbuettel.com/papers/RcppEigen-intro.pdf
 
 // [[Rcpp::export]]
-MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double eps_factor = -1, bool compound=true, bool verbose=true)
+MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound=true, bool verbose=true)
 	{
 	if(type == 1) { return A.inverse(); }
 	if(type == 2)
 		{
 		PartialPivLU<MatrixXd> 	pplu(A); // constructor 
-		if(eps_factor > 0) 
+		if(tol_factor > 0) 
 			{
-			double eps = calculateTolerance(eps_factor, compound, verbose);
-			double eps = std::numeric_limits<double>::epsilon();
-			// according to the docs ... 
-			double eps_old = eps * A.diagonal().size(); 
-			const std::string msg = "The documentation says it is defined as 'diagonal size times machine epsilon' [ NumTraits<Scalar>::epsilon() ]";
-			e calculateTolerance(double old_factor, double new_factor, double eps = 0, const std::string msg="", bool compound=true, bool verbose=true)
-	{
-		eps_factor
-			double eps = calculateTolerance(
-		//if(eps > 0) { pplu.setThreshold(eps); }
-		// pplu.setThreshold(1e-5);
-		/*
-		// diagonal size times machine epsilon.
-	// NumTraits<Scalar>::epsilon()
-	// If you want to come back to the default behavior, call setThreshold(Default_t)
-	*/
+			double eps = calculateTolerance(A, tol_factor, 
+											compound, verbose);
+			// pplu.setThreshold(eps);
+			}
 		return pplu.inverse();
-		}
-	/* if(type == 2) 
-		{ 
-		PartialPivLU<MatrixXd> 	pplu;
-		if(eps > 0) { pplu.setThreshold(eps); }		
-		pplu.compute(A);
-		return pplu(A).inverse();
 		}
 	if(type == 3) 
 		{ 
-		FullPivLU<MatrixXd> 	fplu;
-		if(eps > 0) { fplu.setThreshold(eps); }		
-		fplu.compute(A);
-		return fplu(A).inverse(); 
+		FullPivLU<MatrixXd> 	fplu(A); // constructor 
+		if(tol_factor > 0) 
+			{
+			double eps = calculateTolerance(A, tol_factor, 
+											compound, verbose);
+			// fplu.setThreshold(eps);
+			}
+		return fplu.inverse(); 
 		}
- */
+ 
 	return A.inverse();
 	}
+
+/*
+// [[Rcpp::export]]
+MatrixXd matrix_determinant(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound=true, bool verbose=true)
+	{
+	if(type == 1) { return A.determinant(); }
+	if(type == 2)
+		{
+		PartialPivLU<MatrixXd> 	pplu(A); // constructor 
+		if(tol_factor > 0) 
+			{
+			double eps = calculateTolerance(A, tol_factor, 
+											compound, verbose);
+			// pplu.setThreshold(eps);
+			}
+		return pplu.determinant();
+		}
+	if(type == 3) 
+		{ 
+		FullPivLU<MatrixXd> 	fplu(A); // constructor 
+		if(tol_factor > 0) 
+			{
+			double eps = calculateTolerance(A, tol_factor, 
+											compound, verbose);
+			// fplu.setThreshold(eps);
+			}
+		return fplu.determinant(); 
+		}
+ 
+	return A.determinant();
+	}
+
+*/
 
 
 // make other functions cmatrix_diagonal ... etc. 
