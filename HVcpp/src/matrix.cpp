04c95ec4446bd0b6c9817f3108941244b0fab805
MonteShaffer <monte.shaffer@gmail.com>
2022-10-02 19:10:20 -0400

    Update matrix.cpp
    semi positive definite ... symmetric

--- HVcpp/src/matrix.cpp
+++ HVcpp/src/matrix.cpp
@@ -6,8 +6,12 @@ using Eigen::MatrixXd;			// variable size matrix, double precision
 using Eigen::MatrixXcd;			// ABOVE, complex 
 using Eigen::VectorXd;			// variable size vector, double precision
 using Eigen::VectorXcd;			// ABOVE, complex 
-using Eigen::FullPivLU;
 using Eigen::PartialPivLU;
+using Eigen::FullPivLU;
+
+using Eigen::LLT;
+using Eigen::LDLT;
+
 
 
 //  Rcpp::sourceCpp("matrix.cpp", verbose=TRUE);
@@ -31,6 +35,10 @@ MatrixXd matrix_transpose(Map<MatrixXd> M)
 	// return M.transposeInPlace();
 	return M.transpose();  
 	}
+	
+
+	
+	
 
 	
 // [[Rcpp::export]]
@@ -76,10 +84,10 @@ MatrixXd matrix_identity(int size = 5)
 
 
 double calculateTolerance(Map<MatrixXd> A, double new_factor, 
-							bool compound=true, bool verbose=true)
+							bool compound = true, bool verbose = true)
 {
 	double eps 			= std::numeric_limits<double>::epsilon();
-	int old_factor 		= A.diagonal().size(); 
+	int old_factor 		= A.diagonalSize(); 
 	std::string msg 	= "RTM: *matrix eps* it is defined as 'diagonal size times machine epsilon' \n\t\t\t\t[ setThreshold( ... NumTraits<Scalar>::epsilon() ]";
 	std::string more 	= ""; 
 	if(compound) { more = " * " + std::to_string(old_factor); }
@@ -122,10 +130,130 @@ double calculateTolerance(Map<MatrixXd> A, double new_factor,
 }
 
 
+
+
+// [[Rcpp::export]]	
+bool is_symmetric(Map<MatrixXd> M, double tol_factor = -1, bool compound = true, bool verbose = true)
+	{
+	// update TOLERANCE ??? 
+	// https://stackoverflow.com/a/71827895/184614
+	return M.isApprox( M.transpose() );		
+	}
+
+
+	
+// [[Rcpp::export]]	
+bool is_positive_semi_definite(Map<MatrixXd> M, double tol_factor = -1, bool compound = true, bool verbose = true)
+	{
+	bool symm = is_symmetric(M, tol_factor, compound, verbose);
+	if(!symm) { return false; }
+
+	LDLT<MatrixXd> 	ldlt(M); // constructor 
+	
+	if (ldlt.info() == Eigen::NumericalIssue || !ldlt.isPositive()) 
+		{
+		return false;
+		}
+		
+	return true;
+	}
+		
+// [[Rcpp::export]]	
+bool is_positive_definite(Map<MatrixXd> M, double tol_factor = -1, bool compound = true, bool verbose = true)
+	{
+	bool symm = is_symmetric(M, tol_factor, compound, verbose);
+	if(!symm) { return false; }
+
+	LDLT<MatrixXd> 	ldlt(M); // constructor 
+	
+	return ldlt.isPositive();
+ 	
+	/*
+
+LLT<MatrixXd> 	llt(A); // constructor 
+
+selfadjointView<Eigen::Upper>().ldlt();	
+
+if (ldlt.info() == Eigen::NumericalIssue || !ldlt.isPositive()) 
+		{
+		return false;
+		}
+	return true;
+	
+Eigen::LLT<Eigen::MatrixXd> A_llt(A);
+
+	const auto ldlt = A.template selfadjointView<Eigen::Upper>().ldlt();
+  if (ldlt.info() == Eigen::NumericalIssue || !ldlt.isPositive()) {
+    return false;
+  }
+  
+  */
+	
+	}
+	
+	
 // for SOLVE ... https://dirk.eddelbuettel.com/papers/RcppEigen-intro.pdf
 
+/*
+https://eigen.tuxfamily.org/dox/group__TopicLinearAlgebraDecompositions.html
+
+type ... decomposition types 
+
+type == 1 is default  Matrix::inverse() ... base forumula  
+
+2 .... PartialPivLU	Invertible	Fast	Depends on condition number	-	-	Yes	Excellent	
+Blocking, Implicit MT
+
+3 ... FullPivLU	-	Slow	Proven	Yes	-	Yes	Excellent	
+-
+
+4 ... HouseholderQR	-	Fast	Depends on condition number	-	Orthogonalization	Yes	Excellent	
+Blocking
+
+5 ... ColPivHouseholderQR	-	Fast	Good	Yes	Orthogonalization	Yes	Excellent	
+-
+
+6 ... FullPivHouseholderQR	-	Slow	Proven	Yes	Orthogonalization	Yes	Average	
+-
+
+7 ... CompleteOrthogonalDecomposition	-	Fast	Good	Yes	Orthogonalization	Yes	Excellent	
+-
+
+8 ... LLT	Positive definite	Very fast	Depends on condition number	-	-	Yes	Excellent	
+Blocking
+
+9 ... LDLT
+
+Rank revealing ... 3, 5, 6, 7 
+
+
+svd/solvers types ... start at 21
+
+
+
+21 ... BDCSVD (divide & conquer)	-	One of the fastest SVD algorithms	Excellent	Yes	Singular values/vectors, least squares	Yes (and does least squares)	Excellent	
+Blocked bidiagonalization
+
+22 ... JacobiSVD (two-sided)	-	Slow (but fast for small matrices)	Proven3	Yes	Singular values/vectors, least squares	Yes (and does least squares)	Excellent	
+R-SVD
+
+23 ... SelfAdjointEigenSolver	Self-adjoint	Fast-average2	Good	Yes	Eigenvalues/vectors	-	Excellent	
+Closed forms for 2x2 and 3x3
+
+24 ... ComplexEigenSolver	Square	Slow-very slow2	Depends on condition number	Yes	Eigenvalues/vectors	-	Average	
+-
+
+25 ... EigenSolver	Square and real	Average-slow2	Depends on condition number	Yes	Eigenvalues/vectors	-	Average	
+-
+
+26 ... GeneralizedSelfAdjointEigenSolver	Square	Fast-average2	Depends on condition number	-	Generalized eigenvalues/vectors	-	Good	
+-
+
+
+*/
+
 // [[Rcpp::export]]
-MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound=true, bool verbose=true)
+MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound = true, bool verbose = true)
 	{
 	if(type == 1) { return A.inverse(); }
 	if(type == 2)
@@ -154,9 +282,8 @@ MatrixXd matrix_inverse(Map<MatrixXd> A, int type = 1, double tol_factor = -1, b
 	return A.inverse();
 	}
 
-/*
 // [[Rcpp::export]]
-MatrixXd matrix_determinant(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound=true, bool verbose=true)
+double matrix_determinant(Map<MatrixXd> A, int type = 1, double tol_factor = -1, bool compound=true, bool verbose=true)
 	{
 	if(type == 1) { return A.determinant(); }
 	if(type == 2)
@@ -185,7 +312,29 @@ MatrixXd matrix_determinant(Map<MatrixXd> A, int type = 1, double tol_factor = -
 	return A.determinant();
 	}
 
-*/
+
+
+
+// [[Rcpp::export]]
+MatrixXd matrix_multiplyN(Map<MatrixXd> A, int n=2, int type = 1, double tol_factor = -1, bool compound = true, bool verbose = true)
+	{	
+	int nrow 	= A.rows();
+	int ncol 	= A.cols();
+	int an 		= abs(n) - 1;
+	
+	MatrixXd B;
+		
+	if(n == 0) 	{ return B.Identity(nrow, ncol);		}
+	if(n <  0) 	{ B = matrix_inverse(A, type, 
+						tol_factor, compound, verbose);	}
+	if(n >  0) 	{ B = A;								}
+	
+	for(int i = 0; i < an; i++)
+		{
+		B = B * B;
+		}		
+	return B;
+	}
 
 
 // make other functions cmatrix_diagonal ... etc. 
